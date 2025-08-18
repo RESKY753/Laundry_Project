23afa4239514eb0fdcfd4a7999b22f47b2db06d9
hilmannurm <nurmaulanahilman1@gmail.com>
2025-08-18 19:16:58 +0700

    menambahkan profil

---
+++ Views/Profil.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Profil</title>
+<link rel="stylesheet" href="../Models/styleprofil.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+
+<!-- HEADER -->
+<div class="header">
+  <h2>Profil Saya</h2>
+</div>
+
+<!-- FOTO PROFIL -->
+<div class="profile-photo">
+  <img src="bugatti.jpg">
+</div>
+
+<!-- INFO PROFIL -->
+<div class="profile-info">
+  <div class="info-item">
+    <strong>Nama</strong>
+    <p>Hilman Nur Maulana</p>
+  </div>
+  <div class="info-item">
+    <strong>Email</strong>
+    <p>hilman@gmail.com</p>
+  </div>
+  <div class="info-item">
+    <strong>Telepon</strong>
+    <p>+62 812 3456 7890</p>
+  </div>
+  <div class="info-item">
+    <strong>Alamat</strong>
+    <p>Jl. Raya Batujajar,Kec. Padalarang,Kab.  Bandung Barat</p>
+  </div>
+</div>
+
+<!-- AKSI -->
+<div class="profile-actions">
+  <button class="btn-action"><i class="fa-solid fa-pen"></i> Edit Profil</button>
+  <button class="btn-action logout"><i class="fa-solid fa-right-from-bracket"></i> Keluar</button>
+</div>
+
+<!-- NAVBAR -->
+<div class="navbar">
+  <a href="#home" class="nav-item"><i class="fa-solid fa-house"></i>Home</a>
+  <a href="#notif" class="nav-item"><i class="fa-solid fa-bell"></i>Notifikasi</a>
+  <a href="#profil" class="nav-item"><i class="fa-solid fa-user"></i>Profil</a>
+</div>
+
+</body>
+</html>
